ad5ab80fcbe3b4622043aff26379e5774aaed5a1
Aceproyectos <115713715+Aceproyectos@users.noreply.github.com>
2023-07-04 06:13:20 -0500

    se comento una parte del codigo

--- server/controller/tasks.controller.js
+++ server/controller/tasks.controller.js
@@ -2,15 +2,17 @@ import pkg from "express/lib/response.js";
 const { status } = pkg;
 import { pool } from "../db.js";
 
+//En esta funcion se esta haciendo la consulta de todos los clientes en la base de datos
 export const getTasks = async (req, res) => {
   const [result] = await pool.query("SELECT * FROM cliente");
   res.json(result);
 };
-
+//Se esta haciendo consulta idividual por el ID que le pasen por params
 export const getTask = async (req, res) => {
   const [result] = await pool.query("SELECT * FROM cliente WHERE id = ?", [
     req.params.id,
   ]);
+  //si no se encuentra nada con el ID  que nos mandan se manda un 404
   if (result.length === 0) {
     return res.status(404).json({ message: "Task not found" });
   }
@@ -18,16 +20,17 @@ export const getTask = async (req, res) => {
   res.json(result[0]);
 };
 
+//Se esta insertando nuvos clientes en la base de datos
 export const createTasks = async (req, res) => {
   const { nombre, apellido, edad } = req.body;
   const [results] = await pool.query(
     "INSERT INTO cliente( nombre, apellido, edad) VALUES (?,?,?)",
     [nombre, apellido, edad]
   );
-  console.log(results);
+  //insertID es para saber con que ID se ingresando el cliente
   res.json({ id: results.insertId, nombre, apellido, edad });
 };
-
+//Se esta actualizando la informacion de los clientes se selecciona cliente depende por ID
 export const updateTasks = async (req, res) => {
   const { nombre, apellido, edad } = req.body;
   const [result] = await pool.query("UPDATE cliente SET ? WHERE id = ?", [
@@ -36,7 +39,7 @@ export const updateTasks = async (req, res) => {
   ]);
   res.json(result)
 };
-
+//Se estan eliminando los cliente dependiendo del ID que nos manden
 export const deleteTasks = async (req, res) => {
   const [result] = await pool.query("DELETE FROM cliente WHERE id = ?", [
     req.params.id,
